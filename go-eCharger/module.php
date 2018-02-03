2b6eaea363039963499bb316ac8d54bb45df1421
IPSCoyote <33869299+IPSCoyote@users.noreply.github.com>
2018-02-03 18:03:19 +0100

    Update module.php

--- go-eCharger/module.php
+++ go-eCharger/module.php
@@ -22,7 +22,7 @@
             
           $this->Update();
         }
- 
+
         //=== Modul Funktionen =========================================================================================
         /* Own module functions called via the defined prefix GOeCharger_* 
         *
@@ -48,7 +48,7 @@
           $connectionOK = true;
             
           // get Status for go-eCharger 1
-          if (filter_var($IPAddress, FILTER_VALIDATE_IP)) {
+          if (filter_var($IPAddress, FILTER_VALIDATE_IP) and ping( $IPAddress, 80, 1 )) {
             try {  
                 $ch    = curl_init("http://".$IPAddress."/status"); 
                 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0); 
@@ -78,5 +78,22 @@
           }
             
         }
+        
+        //=== Modul Funktionen =========================================================================================
+        /* Own module functions called via the defined prefix GOeCharger_* 
+        *
+        * GOeCharger_CheckConnection($id);
+        *
+        */
+        
+        function ping($host, $port, $timeout) 
+        { 
+          ob_start();
+          $fP = fSockOpen($host, $port, $errno, $errstr, $timeout); 
+          ob_clean();
+          if (!$fP) { return false; } 
+          return true; 
+        }
+        
     }
 ?>
