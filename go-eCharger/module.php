f85f3f847a798482f03c7b30b5e9138f27446c13
IPSCoyote <33869299+IPSCoyote@users.noreply.github.com>
2018-02-04 18:29:50 +0100

    Update module.php

--- go-eCharger/module.php
+++ go-eCharger/module.php
@@ -62,6 +62,39 @@
           return true;
         }
        
+        public function SetMaxAmpere(int $Ampere) {
+            // Check input value
+            if ( $Ampere < 6 or $Ampere > 32 ) { return false; }
+            
+            // Check requested Ampere is <= max Ampere set in Instance
+            if ( $Ampere > $this->ReadPropertyString("MaxAmpCharger") ) { return false };
+            
+            // first calculate the Button values
+            $button['0'] = 6; // min. Value
+            $gaps = round( ( ( $Ampere - 6 ) / 4 ) - 0.5 );
+            $button['1'] = $button['0'] + $gaps;
+            $button['2'] = $button['1'] + $gaps;
+            $button['3'] = $button['2'] + $gaps;
+            $button['4'] = $Ampere; // max. Value
+            
+            // set values to Charger
+            // set button values
+            setValueToeCharger( 'al1', $button['al1'] );
+            setValueToeCharger( 'al2', $button['al2'] );
+            setValueToeCharger( 'al3', $button['al3'] );
+            setValueToeCharger( 'al4', $button['al4'] );
+            setValueToeCharger( 'al5', $button['al5'] );
+            
+            // set max available Ampere
+            $eChargerStatus = setValueToeCharger( 'ama', $Ampere );
+            
+            // set current available Ampere (if too high)
+            if ( $eChargerStatus->{'amp'} > $eChargerStatus->{'ama'} ) {
+              // set current available to max. available, as current was higher than new max.
+              setValueToeCharger( 'amp', $eChargerStatus->{'ama'} );
+            }        
+        }
+        
         public function SetCurrentAmpere(int $Ampere, boolean $orMaximum ) {
             // Check input value
             if ( $Ampere < 6 or $Ampere > 32 ) { return false; }
