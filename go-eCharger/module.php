1652313c1f2852dc8939c9e02ce98ea0b61563a5
IPSCoyote <33869299+IPSCoyote@users.noreply.github.com>
2018-02-06 16:59:29 +0100

    Update module.php

--- go-eCharger/module.php
+++ go-eCharger/module.php
@@ -410,11 +410,11 @@
               $this->RegisterVariableBoolean("norwayMode", "Erdungsprüfung","~Switch",0);
             }  
             
-            //for($i=1; $i<=10; $i++){
-            //  if ( $this->GetIDForIdent("energyChargedCard".$i) == false ) {
-            //    $this->RegisterVariableFloat("energyChargedCard".$i, "geladene Energie Karte ".$i,"GOECHARGER_Power.1",0);
-            //  }    
-            //}
+            for($i=1; $i<=10; $i++){
+              if ( $this->GetIDForIdent("energyChargedCard".$i) == false ) {
+                $this->RegisterVariableFloat("energyChargedCard".$i, "geladene Energie Karte ".$i,"GOECHARGER_Power.1",0);
+              }    
+            }
         }
     }
 ?>
