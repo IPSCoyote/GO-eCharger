a533046eb9c7c7a04e6884ca1cfb8633e068dd41
IPSCoyote <33869299+IPSCoyote@users.noreply.github.com>
2018-06-24 16:12:44 +0200

    Update module.php

--- go-eCharger/module.php
+++ go-eCharger/module.php
@@ -16,6 +16,8 @@
           $this->RegisterPropertyInteger("MaxAmperage", 6);
           $this->RegisterPropertyInteger("UpdateIdle", 0);  
           $this->RegisterPropertyInteger("UpdateCharging",0); 
+          $this->RegisterPropertyBoolean("AutoReacivation",false); 
+          $this->RegisterPropertyFloat("AverageConsumption",0);
                       
           // Timer
           $this->RegisterTimer("GOeChargerTimer_UpdateTimer", 0, 'GOeCharger_Update($_IPS[\'TARGET\']);');
