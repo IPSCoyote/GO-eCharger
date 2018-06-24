bda251b512a784aa090abd9bbf4c03a4fc519d55
IPSCoyote <33869299+IPSCoyote@users.noreply.github.com>
2018-06-24 17:11:11 +0200

    Update module.php

--- go-eCharger/module.php
+++ go-eCharger/module.php
@@ -131,7 +131,7 @@
             // Handle Re-Activation
             if ( $this->ReadPropertyBoolean("AutoReactivate") == true and
                  GetValueBoolean( $this->GetIDForIdent("accessState") ) == false and    // charger deactivated
-                 GetValueInteger( $this->GetIDForIdent("status") == 3 ) )               // wait for car -> car plugged in
+                 GetValueInteger( $this->GetIDForIdent("status") ) == 3 )               // wait for car -> car plugged in
             {
 
             }
